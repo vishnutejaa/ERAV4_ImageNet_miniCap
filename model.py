fda742d9b3f628f29139e2edfabcc662b22779ff
VENKAT NAREN <52081653+vnaren13@users.noreply.github.com>
2025-11-02 23:28:37 -0600

    Refactor ResNet

--- model.py
+++ model.py
@@ -1,22 +1,23 @@
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
 
 class Bottleneck(nn.Module):
-    expansion = 4  
+    expansion = 4
+
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super().__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1   = nn.BatchNorm2d(planes)
+        self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
                                padding=1, bias=False)
-        self.bn2   = nn.BatchNorm2d(planes)
+        self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes, planes * self.expansion,
                                kernel_size=1, bias=False)
-        self.bn3   = nn.BatchNorm2d(planes * self.expansion)
-        self.relu  = nn.ReLU(inplace=True)
-        self.downsample = downsample  
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+
     def forward(self, x):
         identity = x
 
@@ -37,21 +38,22 @@ class ResNet(nn.Module):
         super().__init__()
         self.inplanes = 64
 
-
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1   = nn.BatchNorm2d(64)
-        self.relu  = nn.ReLU(inplace=True)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        self.layer1 = self._make_layer(block,  64, layers[0], stride=1)  # 3
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)  # 4
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)  # 6
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)  # 3
-
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc      = nn.Linear(512 * block.expansion, num_classes)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
 
+        self._init_weights()
+
+    def _init_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
@@ -60,7 +62,6 @@ class ResNet(nn.Module):
                 nn.init.zeros_(m.bias)
 
     def _make_layer(self, block, planes, blocks, stride):
-
         downsample = None
         out_channels = planes * block.expansion
 
@@ -72,29 +73,26 @@ class ResNet(nn.Module):
 
         layers = []
         layers.append(block(self.inplanes, planes, stride=stride, downsample=downsample))
-        self.inplanes = out_channels 
+        self.inplanes = out_channels
         for _ in range(1, blocks):
             layers.append(block(self.inplanes, planes))
 
         return nn.Sequential(*layers)
 
     def forward(self, x):
-      
-        x = self.relu(self.bn1(self.conv1(x)))   # /2
-        x = self.maxpool(x)                      # /4
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.maxpool(x)
 
-        # Stages
-        x = self.layer1(x)  # conv2_x
-        x = self.layer2(x)  # conv3_x
-        x = self.layer3(x)  # conv4_x
-        x = self.layer4(x)  # conv5_x
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
 
-        # Head
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
         return x
 
+
 def resnet50(num_classes=1000):
-    # ResNet-50 uses [3, 4, 6, 3] bottleneck blocks
     return ResNet(Bottleneck, [3, 4, 6, 3], num_classes=num_classes)
